2ee522d815c36e4e88759133fe92e777853f149f
capyyimg <79929439+capyyimg@users.noreply.github.com>
2021-03-10 13:33:55 -0500

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,16 @@
+input.onButtonPressed(Button.A, function () {
+    Guess += 1
+})
+input.onButtonPressed(Button.AB, function () {
+    if (false || false) {
+    	
+    }
+})
+input.onButtonPressed(Button.B, function () {
+    Guess += -1
+})
+let Acctural_num = randint(0, 20)
+let Guess = 0
+basic.forever(function () {
+	
+})
